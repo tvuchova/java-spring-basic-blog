35d6ea82f79b3cb7a268ca5539ef7a467baa761c
tvuchova <temenuzhka.vuchova@tick42.com>
2020-02-18 09:34:53 +0200

    adds posts

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,14 +1,30 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
-public class BlogController {
+public class BlogController
+{
+
+    private PostRepository postRepository;
+
+    public BlogController(final PostRepository postRepository)
+    {
+        this.postRepository = postRepository;
+    }
+
     @RequestMapping("/")
-    public String listPosts(ModelMap model){
+    public String listPosts(ModelMap model)
+    {
+        List<Post> allPosts = postRepository.getAllPosts();
         model.put("title", "Blog Post 1");
-      return "home";
+        model.put("posts", allPosts);
+        return "home";
     }
 }
